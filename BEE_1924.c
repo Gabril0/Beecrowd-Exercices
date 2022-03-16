872b5a5fcdc2f40e051737b67a0a137def98a86c
Gabril <62163345+Gabril0@users.noreply.github.com>
2022-03-15 22:39:13 -0300

    Add files via upload

---
+++ BEE_1924.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    int cases;
+    int i;
+    char op[30];
+    scanf("%d",&cases);
+    for(i=0;i<cases;i++){
+        scanf("%s",op);     
+    }
+    printf("Ciencia da Computacao\n");
+    return 0;
+}
